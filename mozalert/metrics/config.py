d493fa47ff90bddb53f14130c54dc4bdad33043e
Adam Frank <afrank@mozilla.com>
2020-07-06 07:34:11 -0700

    adding metrics mozalert_check_get_time and mozalert_check_node_time

--- mozalert/metrics/config.py
+++ mozalert/metrics/config.py
@@ -1,5 +1,7 @@
 
 MetricsConfig = {
+    "mozalert_check_get_time": {"type": "Gauge", "labels": ["status", "escalated"],},
+    "mozalert_check_node_time": {"type": "Gauge", "labels": ["status", "escalated"],},
     "mozalert_check_runtime": {"type": "Gauge", "labels": ["status", "escalated"],},
     "mozalert_check_total_time": {"type": "Gauge", "labels": ["status", "escalated"],},
     "mozalert_check_latency": {"type": "Gauge", "labels": ["status", "escalated"],},
