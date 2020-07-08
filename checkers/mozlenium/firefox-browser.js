2c255e4cc8149df0cffe0b0b4c062bea297a1699
Adam Frank <afrank@mozilla.com>
2020-07-08 14:23:37 -0700

    changes to support batman's mozlenium refactoring

--- checkers/mozlenium/firefox-browser.js
+++ checkers/mozlenium/firefox-browser.js
@@ -5,7 +5,7 @@ const $firefox = require('selenium-webdriver/firefox');
 // Create Options to be sent to Firefox
 var options = new $firefox.Options();
 
-// options.addArguments('-headless');
+options.addArguments('-headless');
 options.addArguments('--window-size=1024,768');
 options.addArguments('--disable-gpu');
 options.addArguments('--test-type');
