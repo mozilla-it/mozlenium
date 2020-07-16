32b8d5b43087d5002e318eb1257408d73cf35197
Andrew Sunada <asunada@mozilla.com>
2020-07-16 15:17:34 -0700

    adding cli check for browser

--- checkers/mozlenium/check.js
+++ checkers/mozlenium/check.js
@@ -2,7 +2,6 @@ async function runner($browser, $driver, $secure) {
   //demo check
   var assert = require('assert');
   var url = 'https://duckduckgo.com'
-  console.log("starting check");
   $browser.get(url).then(function(){
       assert.ok($browser.waitForAndFindElement($driver.By.id("content_homepage"), 30000), "Doesn't seem like things are working properly");
       console.log("well that went great");
--- checkers/mozlenium/firefox-browser.js
+++ checkers/mozlenium/firefox-browser.js
@@ -1,11 +1,13 @@
 // Build Firefox Driver for testing
 const $driver = require('selenium-webdriver');
 const $firefox = require('selenium-webdriver/firefox');
-
+const isCLI = process.env.IS_CLI === 'true';
 // Create Options to be sent to Firefox
 var options = new $firefox.Options();
 
-options.addArguments('-headless');
+if (!isCLI) {
+  options.addArguments('-headless');
+}
 options.addArguments('--window-size=1024,768');
 options.addArguments('--disable-gpu');
 options.addArguments('--test-type');
@@ -13,7 +15,11 @@ options.addArguments('--no-sandbox');
 options.addArguments('--disable-dev-shm-usage');
 
 // Create firefox browser object
-module.exports = new $driver.Builder()
-  .forBrowser('firefox')
-  .setFirefoxOptions(options)
-  .build();
+if (!isCLI) {
+  module.exports = new $driver.Builder()
+    .forBrowser('firefox')
+    .setFirefoxOptions(options)
+    .build();
+} else {
+  module.exports = new $driver.Builder().forBrowser('firefox').build();
+}
