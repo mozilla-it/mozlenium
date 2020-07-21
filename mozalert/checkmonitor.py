0cff909b812aaaf50b015fa02c6e26bfdf8e7eaf
Adam Frank <afrank@mozilla.com>
2020-07-21 12:19:34 -0700

    adding circuit breaker counter and log output

--- mozalert/checkmonitor.py
+++ mozalert/checkmonitor.py
@@ -20,6 +20,13 @@ class CheckMonitor(threading.Thread):
 
         self.shutdown = kwargs.get("shutdown", lambda: False)
 
+        # max failures before considering the check run a failure
+        self.failed_threshold = kwargs.get("failed_threshold", 0)
+
+        self.sequential_failed_run_threshold = kwargs.get("sequential_failed_run_threshold", 2)
+
+        self.sequential_failed_runs = 0
+
         self.setName("check-monitor")
 
     def terminate(self):
@@ -85,8 +92,17 @@ class CheckMonitor(threading.Thread):
             else:
                 success += 1
 
+        if failed > self.failed_threshold:
+            self.sequential_failed_runs += 1
+            logging.warning(f"Sequential Failed Sanity Checks: {self.sequential_failed_runs}")
+        else:
+            self.sequential_failed_runs = 0
+
         logging.debug(
             f"sanity check finished with {success} success and {failed} failures"
         )
+
+        if self.sequential_failed_runs > self.sequential_failed_run_threshold:
+            logging.error(f"Circuit breaker triggered: Sanity Check Sequential Failures {self.sequential_failed_runs} > {self.sequential_failed_run_threshold}")
 
         logging.info("Check Monitor finished")
