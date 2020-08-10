56c9dad34271ce2256c173e71b9649577e678542
Adam Frank <afrank@mozilla.com>
2020-08-10 09:18:05 -0700

    improving escalation output

--- mozalert/escalations/email.py
+++ mozalert/escalations/email.py
@@ -5,30 +5,30 @@ from mozalert.utils.sendgrid import SendGridTools
 
 
 class Escalation(BaseEscalation):
-    def __init__(self, name, status, **kwargs):
-        super().__init__(name, status, **kwargs)
+    def __init__(self, name, status, config, args):
+        super().__init__(name, status, config, args)
         self.email = self.args.get("email")
         self.api_key = os.environ.get("SENDGRID_API_KEY", "")
         self.from_email = "Mozalert <afrank+mozalert@mozilla.com>"
         self.message = f"""
             <p>
             <b>Name:</b> {self.name}<br>
-            <b>Status:</b> {self.status}<br>
+            <b>Status:</b> {self.status.status.name}<br>
             """
-        if self.attempt and self.max_attempts:
+        if self.status.attempt and self.config.max_attempts:
             self.message += (
-                "\n" + f"<b>Attempt:</b> {self.attempt}/{self.max_attempts}<br>"
+                "\n" + f"<b>Attempt:</b> {self.status.attempt}/{self.config.max_attempts}<br>"
             )
-        elif self.attempt:
-            self.message += "\n" + f"<b>Attempt:</b> {self.attempt}<br>"
-        if self.last_check:
-            self.message += "\n" + f"<b>Last Check:</b> {self.last_check}<br>"
-        if self.logs:
+        elif self.status.attempt:
+            self.message += "\n" + f"<b>Attempt:</b> {self.status.attempt}<br>"
+        if self.status.last_check:
+            self.message += "\n" + f"<b>Last Check:</b> {self.status.last_check}<br>"
+        if self.status.logs:
             self.message += (
-                "\n" + f"<b>More Details:</b><br> <pre>{self.logs}</pre><br>"
+                "\n" + f"<b>More Details:</b><br> <pre>{self.status.logs}</pre><br>"
             )
         self.message += "\n" + "</p>"
-        self.subject = f"Mozalert {self.status}: {self.name}"
+        self.subject = f"Mozalert {self.status.status.name}: {self.name}"
 
     def run(self):
         SendGridTools.send_message(
